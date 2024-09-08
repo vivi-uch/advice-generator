32c0a39c9dfeb288f7581eb71286c141bc8a4e55
vivi-uch <vivianokonkwouch>
2024-09-08 21:07:38 +0100

    checking

--- src/components/DiceButton.jsx
+++ src/components/DiceButton.jsx
@@ -1,7 +1,7 @@
 function DiceButton({ GetAdvice }) {
   return (
     <button
-      className="rounded-full bg-neonGreen h-16 w-16 flex justify-center items-center shadow-lg hover:shadow-xl absolute bottom-[-52px] left-28 md:left-40"
+      className="rounded-full bg-neonGreen h-16 w-16 flex justify-center items-center shadow-lg hover:shadow-xl absolute bottom-[-52px] left-32 md:left-40"
       onClick={GetAdvice}
     >
       <img className="h-8 w-8" src="images/icon-dice.svg" alt="Dice Icon" />
